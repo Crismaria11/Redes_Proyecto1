4833b86d60f58b4205b3f40cafde19cef075edda
Cristina B <bau161260@uvg.edu.gt>
2021-08-12 12:23:36 -0600

    login, signup, showUsers, addUser, showDetails, deleteAccount, signout

--- project.py
+++ project.py
@@ -25,6 +25,62 @@ class Client(slixmpp.ClientXMPP):
   async def sessionStart(self, e):
     self.send_presence()
     await self.get_roster()
+
+    # Mostrar contactos
+    # referencia de la libreria sleekmpp https://github.com/jplevyak/xmppbroadcast/blob/master/xmppbroadcast.py
+    def mostrarContactos():
+      print("\n")
+      contacts = self.client_roster.groups()
+      # print(contacts)
+      for contact in contacts:
+        print('***************Listado********************')
+        for jid in contacts[contact]:
+          user = self.client_roster[jid]['name']
+          if self.client_roster[jid]['name']:
+            print('\n', user, ' (',jid,')')
+          else:
+            print('\n', jid)
+
+          connecciones = self.client_roster.presence(jid)
+          for res, pres in connecciones.items():
+            show = 'available'
+            if pres['show']:
+              show = pres['show']
+            print('   - ',res, '(',show,')')
+            if pres['status']:
+              print('       ', pres['status'])
+      print("")
+      print('*************Fin Listado******************')
+      print("Done!")
+
+    # Agregar un nuevo contacto
+    def nuevoContacto():
+      nuevoContacto = input("Ingrese userid: ")
+      self.send_presence_subscription(pto=nuevoContacto)
+      mensaje="Buenas buenas!!! Hoy amanecimos..."
+      self.send_message(mto=nuevoContacto, mbody=mensaje, mtype="chat", mfrom=self.boundjid.bare)
+
+    # Mostrar detalles del contacto
+    # referencia de la libreria sleekmpp https://github.com/jplevyak/xmppbroadcast/blob/master/xmppbroadcast.py
+    def detallesContacto():
+      self.get_roster()
+      usedidContact = input("Ingrese userid: ")
+      user = self.client_roster[usedidContact]['name']
+      print('\n %s (%s)' % (user, usedidContact))
+
+      connecciones = self.client_roster.presence(usedidContact)
+      if connecciones == {}:
+        print('       Away')
+      for res, pres in connecciones.items():
+        show = 'available'
+        if pres['show']:
+          show = pres['show']
+        print('   - ', res, ' - ', show)
+        print('       ',  pres['status'])
+
+
+
+
     print("session_start")
     loginStart = True
     while loginStart:
@@ -32,26 +88,26 @@ class Client(slixmpp.ClientXMPP):
       
       Submenu
 
-      1.  Eliminar la cuenta
-      2.  Mostrar usuarios
-      3.  Agregar a un usuario
-      4.  Mostrar detalles de contacto de un usuario
-      5.  Mandar un DM
-      6.  Chat grupal
-      7.  Cambiar mensaje de presencia
-      8.  Enviar archivo
-      9.  Enviar notificaciones
+      1.  Mostrar usuarios
+      2.  Agregar a un usuario
+      3.  Mostrar detalles de usuario
+      4.  Mandar un DM
+      5.  Chat grupal
+      6.  Cambiar mensaje de presencia
+      7.  Enviar archivo
+      8.  Enviar notificaciones
+      9.  Eliminar la cuenta
       10. Salir de la sesion
       
       
       """)
       loginOption = int(input("Que opcion desea realizar? "))
       if loginOption == 1:
-        pass
+        mostrarContactos()
       elif loginOption == 2:
-        pass
+        nuevoContacto()
       elif loginOption == 3:
-        pass
+        detallesContacto()
       elif loginOption == 4:
         pass
       elif loginOption == 5:
@@ -63,20 +119,36 @@ class Client(slixmpp.ClientXMPP):
       elif loginOption == 8:
         pass
       elif loginOption == 9:
-        pass
+        self.register_plugin('xep_0030') 
+        self.register_plugin('xep_0004')
+        self.register_plugin('xep_0077')
+        self.register_plugin('xep_0199')
+        self.register_plugin('xep_0066')
+
+        eliminar = self.Iq()
+        eliminar['type'] = 'set'
+        eliminar['from'] = self.boundjid.user
+        eliminar['register']['remove'] = True
+        print('*************Eliminado******************')
+        eliminar.send()
+        
+        self.disconnect()
+
       elif loginOption == 10:
+        self.disconnect()
         loginStart = False
+
       else:
         print("Por favor escoje una opcion del menu")
 
 
   async def registrar(self, iq):
     self.send_presence()
-    await self.get_roster()
+    self.get_roster()
 
     resp = self.Iq()
     resp['type'] = 'set'
-    resp['register']['userid'] = self.boundjid.user
+    resp['register']['username'] = self.boundjid.user
     resp['register']['password'] = self.password
 
     try:
@@ -94,14 +166,14 @@ def registrar(userid, password):
   cliente = Client(userid, password)
   cliente.register_plugin("xep_0030")
   cliente.register_plugin("xep_0004")
+  cliente.register_plugin("xep_0077")
   cliente.register_plugin("xep_0199")
   cliente.register_plugin("xep_0066")
-  cliente.register_plugin("xep_0077")
 
   cliente["xep_0077"].force_registration = True
 
   cliente.connect()
-  cliente.process()
+  cliente.process(forever=False)
 
   print("Registro exitoso!")
   
@@ -113,7 +185,7 @@ def iniciarSesion(userid, password):
   cliente.connect()
   cliente.process(forever=False)
 
-  print("\nInicio de sesion exitoso!")
+  # print("\nInicio de sesion exitoso!")
 
 
 
